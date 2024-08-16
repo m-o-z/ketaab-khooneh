d4f15dccbf0bc9a03d203c07cafbcece6c4a98f6
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2024-08-16 23:38:37 +0330

    feat: Refine user preview cmp

--- src/components/user/UserPreview/UserPreview.tsx
+++ src/components/user/UserPreview/UserPreview.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { User } from "@/types";
-import { Avatar, Flex, Text } from "@mantine/core";
+import { Avatar, Button, Flex, Text, useMantineTheme } from "@mantine/core";
 import Link from "next/link";
 import { getUserFullName } from "@/app/utils/users";
+import { capitalizeName } from "@/utils/name";
 
 type Props = {
   user: User;
+  color: string;
 };
-const UserPreview = ({ user }: Props) => {
-  const fullName = getUserFullName(user);
+const UserPreview = ({ user, color }: Props) => {
+  const theme = useMantineTheme();
+  const fullName = capitalizeName(getUserFullName(user));
   return (
     <Link href={`/user/${user.id}`}>
-      <Flex gap="xs">
-        <Avatar src={user.image} size="sm" name={fullName} color="initials" />
-        <Text size="sm">{fullName}</Text>
-      </Flex>
+      <Button
+        color={color ?? theme.colors.gray[5]}
+        size="compact-md"
+        autoContrast
+        variant="transparent"
+      >
+        <Flex gap="xs" align={"center"}>
+          <Avatar src={user.image} size="sm" name={fullName} color="initials" />
+          <Text>{fullName}</Text>
+        </Flex>
+      </Button>
     </Link>
   );
 };
