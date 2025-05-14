329af48f6077376f2cc6d0d45a7fcc7ad9e9e802
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2025-05-14 18:02:30 +0330

    build: add logger

--- src/client/pbClient.ts
+++ src/client/pbClient.ts
@@ -1,5 +1,8 @@
 import PocketBase from "pocketbase";
 
+console.log({
+  URL: process.env.NEXT_PUBLIC_POCKETBASE_URL,
+});
 const pbClient = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
 export default pbClient;
