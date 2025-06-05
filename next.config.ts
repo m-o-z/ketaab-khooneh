02e13de3779a1401daea41bf43a998248c0c59a2
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2025-06-05 23:57:29 +0330

    feat: add pwa configs

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+import withPWA from "@ducanh2912/next-pwa";
+import { NextConfig } from "next";
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+} as NextConfig;
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
