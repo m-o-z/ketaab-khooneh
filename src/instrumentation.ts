aaa921d8de7249b677b32ad450ea2d23c74dab8a
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2025-06-12 11:55:50 +0330

    ci: update config for getting env values in github action

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
-  if (typeof window === "undefined") {
-    import("@/lib/redis");
-    import("@/setup/index");
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("@/lib/redis");
+    await import("@/setup/index");
   }
 }
