1807b8d483aba5058e95812eca690812c88de392
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2025-06-12 04:55:11 +0330

    build: resolve build errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 import withPWA from "@ducanh2912/next-pwa";
+import { isServer } from "@tanstack/react-query";
 import { NextConfig } from "next";
 
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals.concat([
+        "dns",
+        "net",
+        "tls",
+        "string_decoder",
+        "stream",
+        "crypto",
+      ]);
+    }
+
+    return config;
   },
 } as NextConfig;
 
